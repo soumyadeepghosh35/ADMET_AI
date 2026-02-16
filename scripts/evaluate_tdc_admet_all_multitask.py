e75dc8ea86168c6e892fc044cb08872a9e1520e2
Kyle Swanson <swansonk.14@gmail.com>
2026-02-16 14:31:17 -0800

    Updated evaluation script

--- scripts/evaluate_tdc_admet_all_multitask.py
+++ scripts/evaluate_tdc_admet_all_multitask.py
@@ -1,7 +1,9 @@
 """Evaluate predictions from multitask Chemprop models on all TDC ADMET datasets."""
 
+from collections import defaultdict
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 from sklearn.metrics import average_precision_score, roc_auc_score, mean_absolute_error, r2_score
 
@@ -15,65 +17,73 @@ def evaluate_tdc_admet_all_multitask(data_dir: Path, preds_dir: Path, save_dir: 
     :param preds_dir: A directory containing the predictions from Chemprop models trained on the multitask TDC ADMET datasets.
     :param save_dir: A directory where the evaluation results will be saved.
     """
-    # Get dataset names
-    names = ["admet_regression", "admet_classification"]
-
     # Load data
-    regression_data = pd.read_csv(data_dir / "admet_regression.csv")
-    classification_data = pd.read_csv(data_dir / "admet_classification.csv")
+    regression_data = pd.read_csv(data_dir / "admet_regression.csv").set_index(ADMET_ALL_SMILES_COLUMN)
+    classification_data = pd.read_csv(data_dir / "admet_classification.csv").set_index(ADMET_ALL_SMILES_COLUMN)
 
     # Load predictions for each dataset
-    regression_preds = pd.concat(
-        [pd.read_csv(path) for path in preds_dir.glob("admet_regression/**/test_predictions.csv")]
-    )
-    classification_preds = pd.concat(
-        [pd.read_csv(path) for path in preds_dir.glob("admet_classification/**/test_predictions.csv")]
-    )
+    regression_preds_list = [
+        pd.read_csv(path).set_index(ADMET_ALL_SMILES_COLUMN)
+        for path in preds_dir.glob("admet_regression/**/test_predictions.csv")
+    ]
+    classification_preds_list = [
+        pd.read_csv(path).set_index(ADMET_ALL_SMILES_COLUMN)
+        for path in preds_dir.glob("admet_classification/**/test_predictions.csv")
+    ]
 
-    # Average predictions across replicates
-    regression_preds = regression_preds.groupby("smiles").mean()
-    classification_preds = classification_preds.groupby("smiles").mean()
-
-    # Set index to SMILES
-    regression_data.set_index(ADMET_ALL_SMILES_COLUMN, inplace=True)
-    classification_data.set_index(ADMET_ALL_SMILES_COLUMN, inplace=True)
-
-    # Subset data to only include molecules with predictions
-    regression_data = regression_data.loc[regression_preds.index]
-    classification_data = classification_data.loc[classification_preds.index]
-
-    # Evaluate predictions and save results
+    # Set up save directory
     save_dir.mkdir(parents=True, exist_ok=True)
 
-    classification_results = []
-    for column in classification_data.columns:
-        valid_mask = classification_data[column].notna()
-        valid_data = classification_data[valid_mask]
-        valid_preds = classification_preds[valid_mask]
+    # Evaluate predictions for each dataset
+    task_to_results = defaultdict(lambda: defaultdict(list))
 
-        classification_results.append(
-            {
-                "task": column,
-                "roc_auc": roc_auc_score(valid_data[column], valid_preds[column]),
-                "prc_auc": average_precision_score(valid_data[column], valid_preds[column]),
-            }
-        )
+    for classification_preds in classification_preds_list:
+        matched_classification_data = classification_data.loc[classification_preds.index]
+
+        for task in classification_preds.columns:
+            valid_mask = matched_classification_data[task].notna()
+            valid_data = matched_classification_data[valid_mask]
+            valid_preds = classification_preds[valid_mask]
+
+            task_to_results[task]["roc_auc"].append(roc_auc_score(valid_data[task], valid_preds[task]))
+            task_to_results[task]["prc_auc"].append(average_precision_score(valid_data[task], valid_preds[task]))
+
+    task_to_summary_results = {
+        task: {
+            "ROC AUC Mean": np.mean(task_to_results[task]["roc_auc"]),
+            "ROC AUC Std": np.std(task_to_results[task]["roc_auc"]),
+            "PRC AUC Mean": np.mean(task_to_results[task]["prc_auc"]),
+            "PRC AUC Std": np.std(task_to_results[task]["prc_auc"]),
+        }
+        for task in classification_data.columns
+    }
+
+    classification_results = pd.DataFrame(task_to_summary_results).T.reset_index(names="Task")
 
     pd.DataFrame(classification_results).to_csv(save_dir / "admet_classification_results.csv", index=False)
 
-    regression_results = []
-    for column in regression_data.columns:
-        valid_mask = regression_data[column].notna()
-        valid_data = regression_data[valid_mask]
-        valid_preds = regression_preds[valid_mask]
+    for regression_preds in regression_preds_list:
+        matched_regression_data = regression_data.loc[regression_preds.index]
 
-        regression_results.append(
-            {
-                "task": column,
-                "mae": mean_absolute_error(valid_data[column], valid_preds[column]),
-                "r2": r2_score(valid_data[column], valid_preds[column]),
-            }
-        )
+        for task in regression_preds.columns:
+            valid_mask = matched_regression_data[task].notna()
+            valid_data = matched_regression_data[valid_mask]
+            valid_preds = regression_preds[valid_mask]
+
+            task_to_results[task]["mae"].append(mean_absolute_error(valid_data[task], valid_preds[task]))
+            task_to_results[task]["r2"].append(r2_score(valid_data[task], valid_preds[task]))
+
+    task_to_summary_results = {
+        task: {
+            "MAE Mean": np.mean(task_to_results[task]["mae"]),
+            "MAE Std": np.std(task_to_results[task]["mae"]),
+            "R2 Mean": np.mean(task_to_results[task]["r2"]),
+            "R2 Std": np.std(task_to_results[task]["r2"]),
+        }
+        for task in regression_data.columns
+    }
+
+    regression_results = pd.DataFrame(task_to_summary_results).T.reset_index(names="Task")
 
     pd.DataFrame(regression_results).to_csv(save_dir / "admet_regression_results.csv", index=False)
 
