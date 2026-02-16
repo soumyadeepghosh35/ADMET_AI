4a575ed29d610aaaa3bc3b584870c4ca7aa8ae3c
Kyle Swanson <swansonk.14@gmail.com>
2026-02-15 22:01:10 -0800

    Removing TDC dependence from constants file

--- scripts/prepare_tdc_admet_all.py
+++ scripts/prepare_tdc_admet_all.py
@@ -3,16 +3,59 @@
 from pathlib import Path
 
 import pandas as pd
+from tdc.single_pred import ADME, Tox
+from tdc.utils import retrieve_label_name_list
 from tqdm import tqdm
 
 from tdc_constants import (
     ADMET_ALL_SMILES_COLUMN,
     ADMET_GROUP_SMILES_COLUMN,
     ADMET_GROUP_TARGET_COLUMN,
-    DATASET_TO_LABEL_NAMES,
     DATASET_TO_TYPE,
-    TDC_DATASET_TO_CLASS,
 )
+
+
+TDC_DATASET_TO_CLASS = {
+    "Caco2_Wang": ADME,
+    "PAMPA_NCATS": ADME,
+    "HIA_Hou": ADME,
+    "Pgp_Broccatelli": ADME,
+    "Bioavailability_Ma": ADME,
+    "Lipophilicity_AstraZeneca": ADME,
+    "Solubility_AqSolDB": ADME,
+    "HydrationFreeEnergy_FreeSolv": ADME,
+    "BBB_Martins": ADME,
+    "PPBR_AZ": ADME,
+    "VDss_Lombardo": ADME,
+    "CYP2C19_Veith": ADME,
+    "CYP2D6_Veith": ADME,
+    "CYP3A4_Veith": ADME,
+    "CYP1A2_Veith": ADME,
+    "CYP2C9_Veith": ADME,
+    "CYP2C9_Substrate_CarbonMangels": ADME,
+    "CYP2D6_Substrate_CarbonMangels": ADME,
+    "CYP3A4_Substrate_CarbonMangels": ADME,
+    "Half_Life_Obach": ADME,
+    "Clearance_Hepatocyte_AZ": ADME,
+    "Clearance_Microsome_AZ": ADME,
+    "LD50_Zhu": Tox,
+    "hERG": Tox,
+    "herg_central": Tox,
+    "hERG_Karim": Tox,
+    "AMES": Tox,
+    "DILI": Tox,
+    "Skin_Reaction": Tox,
+    "Carcinogens_Lagunin": Tox,
+    "Tox21": Tox,
+    "ToxCast": Tox,
+    "ClinTox": Tox,
+}
+
+DATASET_TO_LABEL_NAMES = {
+    "herg_central": ["hERG_inhib"],
+    "Tox21": retrieve_label_name_list("Tox21"),
+    "ToxCast": retrieve_label_name_list("Toxcast"),
+}
 
 
 def prepare_tdc_admet_all(save_dir: Path, skip_datasets: list[str] = None) -> None:
