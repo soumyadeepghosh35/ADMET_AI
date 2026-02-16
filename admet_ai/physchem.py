32e495e909218c1053ebeb1895adf130605b6248
Kyle Swanson <swansonk.14@gmail.com>
2026-02-15 21:47:22 -0800

    Adding PAINS, BRENK, NIH filters

--- admet_ai/physchem.py
+++ admet_ai/physchem.py
@@ -1,11 +1,10 @@
 """Compute physicochemical properties using RDKit."""
 
-from multiprocessing import Pool
 import pandas as pd
-import numpy as np
 from rdkit import Chem
 from rdkit.Chem.Crippen import MolLogP
 from rdkit.Chem.Descriptors import MolWt
+from rdkit.Chem.FilterCatalog import FilterCatalog, FilterCatalogParams
 from rdkit.Chem.QED import qed
 from rdkit.Chem.rdMolDescriptors import (
     CalcNumAtomStereoCenters,
@@ -15,6 +14,46 @@ from rdkit.Chem.rdMolDescriptors import (
 )
 
 from tqdm import tqdm
+
+
+params_pains = FilterCatalogParams()
+params_pains.AddCatalog(FilterCatalogParams.FilterCatalogs.PAINS)
+catalog_pains = FilterCatalog(params_pains)
+
+params_brenk = FilterCatalogParams()
+params_brenk.AddCatalog(FilterCatalogParams.FilterCatalogs.BRENK)
+catalog_brenk = FilterCatalog(params_brenk)
+
+params_nih = FilterCatalogParams()
+params_nih.AddCatalog(FilterCatalogParams.FilterCatalogs.NIH)
+catalog_nih = FilterCatalog(params_nih)
+
+
+def pains_alert(mol: Chem.Mol) -> int:
+    """Determines how many of the PAINS alert rules are satisfied by the molecule.
+
+    :param mol: An RDKit molecule.
+    :return: The number of PAINS alert rules satisfied by the molecule.
+    """
+    return len(catalog_pains.GetMatches(mol))
+
+
+def brenk_alert(mol: Chem.Mol) -> int:
+    """Determines how many of the BRENK alert rules are satisfied by the molecule.
+
+    :param mol: An RDKit molecule.
+    :return: The number of BRENK alert rules satisfied by the molecule.
+    """
+    return len(catalog_brenk.GetMatches(mol))
+
+
+def nih_alert(mol: Chem.Mol) -> int:
+    """Determines how many of the NIH alert rules are satisfied by the molecule.
+
+    :param mol: An RDKit molecule.
+    :return: The number of NIH alert rules satisfied by the molecule.
+    """
+    return len(catalog_nih.GetMatches(mol))
 
 
 def lipinski_rule_of_five(mol: Chem.Mol) -> float:
@@ -44,6 +83,9 @@ PHYSCHEM_PROPERTY_TO_FUNCTION = {
     "QED": qed,
     "stereo_centers": CalcNumAtomStereoCenters,
     "tpsa": CalcTPSA,
+    "PAINS_alert": pains_alert,
+    "BRENK_alert": brenk_alert,
+    "NIH_alert": nih_alert,
 }
 
 
